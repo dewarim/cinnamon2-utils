bb26ff42d717f631ab3eb0308a6f000a95de2e4e
Ingo Wiarda <ingo_wiarda@dewarim.de>
2012-12-11 18:37:35 +0100

    initial commit to new git repository

---
+++ README.md
@@ -0,0 +1,14 @@
+# CinnamonUtils
+
+CinnamonUtils is a collection of utility classes for the Cinnamon CMS server, version 2.x.
+
+License: LGPL 2.1 or later
+
+To build this library, use the build script from Cinnamon server, which can be found at
+
+* https://github.com/dewarim/cinnamon2-server/blob/master/build.xml
+
+Homepage: http://cinnamon-cms.de
+Sourceforge project page: http://sourceforge.net/projects/cinnamon
+
+Maintainer: ingo.wiarda@horner-project.de
---
+++ src/server/interfaces/Response.java
@@ -0,0 +1,23 @@
+package server.interfaces;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface Response {
+	void setServletResponse(HttpServletResponse resp);
+	HttpServletResponse getServletResponse();
+	
+	/**
+	 * Send the content of the Response to the client which requested it.
+	 * @throws IOException
+	 */
+	void write() throws IOException;
+	
+	/**
+	 * For filters and triggers, this method should return the content
+	 * of the Response object as a String, if possible. A XML-String is
+	 * recommended.
+	 * @return the content of the Response
+	 */
+	String getContent();
+}
